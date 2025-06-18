e0f54eb0b6b6256d922f5b96093cde249cd40155
DakCo <wonjaek36@protonmail.com>
2025-06-18 23:52:36 +0900

    Add version method to Base and Curl components

--- ruby/lib/components/fetch/curl.rb
+++ ruby/lib/components/fetch/curl.rb
@@ -3,6 +3,14 @@ require_relative 'base'
 module Component
   class CurlComponent < BaseComponent
     def exists?
+      system('curl --version > /dev/null 2>&1')
+    end
+
+    def version
+      output = `curl --version 2>&1`
+      output.split[1] if $?.success?
+    rescue Errno::ENOENT
+      nil
     end
   end
 end
