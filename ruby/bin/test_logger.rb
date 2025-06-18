a184d4bca419513dea74228ab73ce8cb0b2a7aa5
Wonjae Kim <wonjaek36@gmail.com>
2025-06-18 23:22:10 +0900

    [#10] feat: create bin dir & add test logger

---
+++ ruby/bin/test_logger.rb
@@ -0,0 +1,16 @@
+#!/usr/bin/env ruby
+require_relative 'bin_helper'
+require 'mixins/loggable'
+
+class TestLogger
+  include Loggable
+
+  def test_log(msg)
+    logger.info(msg)
+  end
+end
+
+if __FILE__ == $0
+  t = TestLogger.new
+  t.test_log("테스트 메시지입니다!")
+end
