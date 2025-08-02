32080e6545c6fd45747d273a7b19da96c1007c80
Wonjae Kim <wonjaek36@protonmail.com>
2025-08-02 10:54:21 +0900

    [#11] fix: only clone powerlevel10k repo for now

--- ruby/lib/components/shell/powerlevel10k.rb
+++ ruby/lib/components/shell/powerlevel10k.rb
@@ -3,9 +3,9 @@ require_relative '../base'
 require_relative '../configuration'
 require_relative '../../mixins/installable'
 require_relative '../../mixins/loggable'
+require_relative '../fetch/git'
 
 module Component
-  # Component for installing the powerlevel10k zsh theme
   class Powerlevel10kComponent < BaseComponent
     include Installable
     include Loggable
@@ -14,19 +14,18 @@ module Component
     REPO_URL = 'https://github.com/romkatv/powerlevel10k.git'
     THEME_DIR = File.join(CONFIG.home, '.oh-my-zsh/custom/themes/powerlevel10k')
     ZSHRC = File.join(CONFIG.home, '.zshrc')
-    LOCAL_P10K = File.expand_path(File.join(File.dirname(__FILE__), '..', '.p10k.zsh'))
-    P10K_LINK = File.join(CONFIG.home, '.p10k.zsh')
+
+    def initialize
+      @git = Component::GitComponent.new
+      @ohmyzsh = Component::OhMyZshComponent.new
+    end
 
     def exists?
-      runCmd('git', '--version')
-      true
-    rescue RuntimeError
-      logger.warn('git command not found')
-      false
+      Dir.exist?(THEME_DIR)
     end
 
     def installed?
-      Dir.exist?(THEME_DIR)
+      self.exists?
     end
 
     def install
@@ -35,70 +34,26 @@ module Component
         return
       end
 
+      install!
+    end
+
+    def install!
+      logger.debug("Check oh-my-zsh installation, #{File.join(CONFIG.home, '.oh-my-zsh')}")
+      if not @ohmyzsh.installed?
+        logger.error('Oh My Zsh is not installed. Please install Oh My Zsh first.')
+        raise 'Oh My Zsh is not installed. Please install Oh My Zsh first.'
+      end
+      FileUtils.mkdir_p(THEME_DIR) unless Dir.exist?(THEME_DIR)
       logger.info('Installing Powerlevel10k theme')
-      runCmd('git', 'clone', '--depth', '1', REPO_URL, THEME_DIR)
-      update_zshrc
-      FileUtils.ln_sf(LOCAL_P10K, P10K_LINK) if File.exist?(LOCAL_P10K)
+      @git.clone(REPO_URL, THEME_DIR)
+      # TODO: COPY .zshrc & .p10k.zsh files in here or not
     rescue => e
       logger.error("Failed to install Powerlevel10k: #{e}")
       raise e
     end
 
     def rollback
-      FileUtils.rm_rf(THEME_DIR)
-      rollback_zshrc
-      FileUtils.rm_f(P10K_LINK) if File.symlink?(P10K_LINK)
-    end
-
-    private
-
-    def update_zshrc
-      return unless File.exist?(ZSHRC)
-
-      content = File.read(ZSHRC)
-      if content =~ /^ZSH_THEME=/
-        content.sub!(/^ZSH_THEME=.*/, 'ZSH_THEME="powerlevel10k/powerlevel10k"')
-      else
-        content << "\nZSH_THEME=\"powerlevel10k/powerlevel10k\"\n"
-      end
-
-      lines = content.lines
-      unless lines.any? { |l| l =~ /^# Enable Powerlevel10k/ }
-        addition = [
-          '# Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.zshrc.',
-          '# Initialization code that may require console input (password prompts, [y/n]',
-          '# confirmations, etc.) must go above this block; everything else may go below.',
-          'if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]; then',
-          '    source "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"',
-          'fi'
-        ]
-        File.open(ZSHRC, 'a') { |f| addition.each { |l| f.puts l } }
-      end
-
-      unless lines.any? { |l| l =~ /^# To customize prompt/ }
-        File.open(ZSHRC, 'a') do |f|
-          f.puts '# To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.'
-          f.puts '[[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh'
-        end
-      end
-
-      File.write(ZSHRC, content)
-    end
-
-    def rollback_zshrc
-      return unless File.exist?(ZSHRC)
-
-      content = File.read(ZSHRC)
-      content.gsub!(/^ZSH_THEME="powerlevel10k\/powerlevel10k"/, 'ZSH_THEME="robbyrussell"')
-      lines = content.lines.reject do |l|
-        l =~ /^# Enable Powerlevel10k/ ||
-        l.include?('p10k-instant-prompt') ||
-        l =~ /^# Initialization code/ ||
-        l =~ /^# confirmations/ ||
-        l =~ /^# To customize prompt/ ||
-        l.include?('p10k.zsh')
-      end
-      File.write(ZSHRC, lines.join)
+      raise NotImplementedError, 'Rollback not implemented for Powerlevel10kComponent'
     end
   end
 end
--- ruby/spec/lib/components/shell/powerlevel10k_spec.rb
+++ ruby/spec/lib/components/shell/powerlevel10k_spec.rb
@@ -1,21 +1,31 @@
 require 'spec_helper'
 require 'components/shell/powerlevel10k'
+require 'components/shell/oh_my_zsh'
+require 'components/fetch/git'
 
 RSpec.describe Component::Powerlevel10kComponent do
   subject(:p10k) { described_class.new }
+  
+  let(:mock_git) { instance_spy(Component::GitComponent) }
+  let(:mock_ohmyzsh) { instance_spy(Component::OhMyZshComponent) }
+  let(:null_logger) { Logger.new(File::NULL) }
+  
   before do
-    null_logger = Logger.new(File::NULL)
     allow(p10k).to receive(:logger).and_return(null_logger)
+    allow(Component::GitComponent).to receive(:new).and_return(mock_git)
+    allow(Component::OhMyZshComponent).to receive(:new).and_return(mock_ohmyzsh)
+    p10k.instance_variable_set(:@git, mock_git)
+    p10k.instance_variable_set(:@ohmyzsh, mock_ohmyzsh)
   end
 
   describe '#exists?' do
-    it 'returns true when git command is available' do
-      allow(p10k).to receive(:runCmd).with('git', '--version').and_return(true)
+    it 'returns true when theme directory exists' do
+      allow(Dir).to receive(:exist?).with(described_class::THEME_DIR).and_return(true)
       expect(p10k.exists?).to be true
     end
 
-    it 'returns false when git command is missing' do
-      allow(p10k).to receive(:runCmd).with('git', '--version').and_raise(RuntimeError)
+    it 'returns false when theme directory does not exist' do
+      allow(Dir).to receive(:exist?).with(described_class::THEME_DIR).and_return(false)
       expect(p10k.exists?).to be false
     end
   end
@@ -36,23 +46,50 @@ RSpec.describe Component::Powerlevel10kComponent do
     context 'when already installed' do
       it 'does nothing' do
         allow(p10k).to receive(:installed?).and_return(true)
+        
         p10k.install
-        expect(p10k).not_to receive(:runCmd)
+        
+        expect(mock_git).not_to have_received(:clone)
       end
     end
 
     context 'when not installed' do
-      it 'clones the repository and updates zshrc' do
+      before do
         allow(p10k).to receive(:installed?).and_return(false)
-        allow(p10k).to receive(:runCmd).with('git', 'clone', '--depth', '1', described_class::REPO_URL, described_class::THEME_DIR).and_return(true)
-        allow(p10k).to receive(:update_zshrc).and_return(true)
-        allow(FileUtils).to receive(:ln_sf)
-        allow(File).to receive(:exist?).with(described_class::LOCAL_P10K).and_return(true)
+      end
 
-        p10k.install
+      context 'when Oh My Zsh is installed' do
+        it 'clones the repository' do
+          allow(mock_ohmyzsh).to receive(:installed?).and_return(true)
+          allow(Dir).to receive(:exist?).with(described_class::THEME_DIR).and_return(false)
+          allow(FileUtils).to receive(:mkdir_p).with(described_class::THEME_DIR)
+          allow(mock_git).to receive(:clone).with(described_class::REPO_URL, described_class::THEME_DIR)
 
-        expect(p10k).to have_received(:runCmd).with('git', 'clone', '--depth', '1', described_class::REPO_URL, described_class::THEME_DIR)
-        expect(p10k).to have_received(:update_zshrc)
+          p10k.install
+          
+          expect(FileUtils).to have_received(:mkdir_p).with(described_class::THEME_DIR)
+          expect(mock_git).to have_received(:clone).with(described_class::REPO_URL, described_class::THEME_DIR)
+        end
+
+        it 'does not create theme directory if it already exists' do
+          allow(mock_ohmyzsh).to receive(:installed?).and_return(true)
+          allow(Dir).to receive(:exist?).with(described_class::THEME_DIR).and_return(true)
+          allow(FileUtils).to receive(:mkdir_p)
+          allow(mock_git).to receive(:clone).with(described_class::REPO_URL, described_class::THEME_DIR)
+          
+          p10k.install
+          
+          expect(FileUtils).not_to have_received(:mkdir_p)
+          expect(mock_git).to have_received(:clone).with(described_class::REPO_URL, described_class::THEME_DIR)
+        end
+      end
+
+      context 'when Oh My Zsh is not installed' do
+        it 'raises an error' do
+          allow(mock_ohmyzsh).to receive(:installed?).and_return(false)
+          
+          expect { p10k.install }.to raise_error(RuntimeError, 'Oh My Zsh is not installed. Please install Oh My Zsh first.')
+        end
       end
     end
   end
