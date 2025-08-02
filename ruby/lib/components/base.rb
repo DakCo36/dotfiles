dccd6eabf58a32cc90a83db9e099f923992d62cc
Wonjae Kim <wonjaek36@protonmail.com>
2025-08-02 23:46:27 +0900

    [#11] feat: define depends_on & install dependencies automatically

--- ruby/lib/components/base.rb
+++ ruby/lib/components/base.rb
@@ -1,12 +1,56 @@
+require 'singleton'
+
 module Component
+  class DependencyError < StandardError; end
+
   # This module defined the interface for an component.
   class BaseComponent
-    def exists?
+    def self.inherited(subclass)
+      super
+      subclass.include Singleton # Make new method available
+    end
+    
+    def exist?
       raise NotImplementedError, "#{self.class} has not implemented method '#{__method__}'"
+    end
+    
+    def available?
+      exist?
     end
 
     def version
       raise NotImplementedError, "#{self.class} has not implemented method '#{__method__}'"
+    end
+    
+    def self.dependencies(&block)
+      @dependencies ||= {}
+      instance_eval(&block) if block_given?
+      @dependencies
+    end
+    
+    def self.depends_on(component_class, name: nil)
+      # 이름이 제공되지 않으면 클래스 이름에서 자동 생성
+      if name.nil?
+        class_name = component_class.name.split('::').last  # Component::GitComponent → GitComponent
+        name = class_name
+          .gsub(/Component$/, '')  # GitComponent → Git
+          .gsub(/([A-Z]+)([A-Z][a-z])/, '\1_\2')  # ABCDef → ABC_Def
+          .gsub(/([a-z\d])([A-Z])/, '\1_\2')  # abcDef → abc_Def
+          .downcase  # Git → git, OhMyZsh → oh_my_zsh
+          .to_sym
+      end
+      
+      dependencies[name] = component_class
+      
+      define_method(name) do
+        instance_variable_name = "@#{name}"
+        instance_variable_get(instance_variable_name)
+        instance_variable_set(instance_variable_name, dependency)
+      end
+    end
+    
+    def dependencies
+      self.class.dependencies
     end
 
     private
