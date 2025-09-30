63db8c098852ee7be14b387827766dfe20d2ae79
Wonjae Kim <wonjaek36@protonmail.com>
2025-09-30 23:34:54 +0900

    [#0] feat(shell): Create test image as dotfiles:shell

--- shell/test/test.sh
+++ shell/test/test.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-docker build --progress plain -t shell-test -f test/Dockerfile .
+docker build --progress plain -t dotfiles:shell -f test/Dockerfile .
+docker image inspect dotfiles:shell >/dev/null && echo "✅ Docker image build: dotfiles:shell" || (echo "❌ Docker image build failed"; exit 1)
+
