9780b99b55d1a716b373d13381ab4e95cc009b88
Wonjae Kim <wonjaek36@gmail.com>
2025-11-07 08:25:50 +0900

    #19: support install command

---
+++ ruby/bin/install.rb
@@ -0,0 +1,9 @@
+#!/usr/bin/env ruby
+require_relative 'bin_helper'
+require 'mixins/loggable'
+require 'components/shell/zsh_binary'
+
+if __FILE__ == $0
+  zsh = Component::ZshBinaryComponent.instance
+  zsh.install
+end
