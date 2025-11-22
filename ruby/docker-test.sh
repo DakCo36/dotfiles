d5a0f14d442e9418474fde930d6276432642d8a6
Wonjae Kim <wonjaek36@protonmail.com>
2025-11-22 15:55:59 +0900

    19: fix space

--- ruby/docker-test.sh
+++ ruby/docker-test.sh
@@ -5,4 +5,4 @@ if [ "$1" = "--no-cache" ]; then
   CACHE_FLAG="--no-cache"
 fi
 
-docker build ${CACHE_FLAG:+$CACHE_FLAG }--progress plain -t dotfiles:ruby -f ./bin/test/Dockerfile .
+docker build ${CACHE_FLAG:+$CACHE_FLAG} --progress plain -t dotfiles:ruby -f ./bin/test/Dockerfile .
