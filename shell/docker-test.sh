334cea1045a7687a3d7ee8055ee770934f689406
Wonjae Kim <wonjaek36@protonmail.com>
2025-12-03 23:04:15 +0900

    chore(19): Remove dotfiles:shell before creating other one

--- shell/docker-test.sh
+++ shell/docker-test.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+docker rmi dotfiles:shell 2>/dev/null || true
+
 docker build --progress plain -t dotfiles:shell -f test/Dockerfile .
 docker image inspect dotfiles:shell >/dev/null && echo "✅ Docker image build: dotfiles:shell" || (echo "❌ Docker image build failed"; exit 1)
 
