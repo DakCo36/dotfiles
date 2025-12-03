35277528b262c11602b70f4609bc7f6a3d1af89e
Wonjae Kim <wonjaek36@protonmail.com>
2025-12-03 23:04:54 +0900

    chore(19): Support --no-cache build

--- shell/docker-test.sh
+++ shell/docker-test.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
+CACHE_FLAG=""
+if [ "$1" = "--no-cache" ]; then
+  CACHE_FLAG="--no-cache"
+fi
+
+echo "Removing existing dotfiles:shell image"
 docker rmi dotfiles:shell 2>/dev/null || true
 
-docker build --progress plain -t dotfiles:shell -f test/Dockerfile .
+docker build ${CACHE_FLAG:+$CACHE_FLAG} --progress plain -t dotfiles:shell -f test/Dockerfile .
 docker image inspect dotfiles:shell >/dev/null && echo "✅ Docker image build: dotfiles:shell" || (echo "❌ Docker image build failed"; exit 1)
 
