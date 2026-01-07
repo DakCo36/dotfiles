47e8ec127234bc2071abc494b0ee9418e5f98ac1
DakCo <wonjaek36@gmail.com>
2026-01-08 08:36:07 +0900

    #19: install zgenom (#31)
    * feat(19): Install zgenom
    
    * chore(19): Remove dotfiles:shell before creating other one
    
    * chore(19): Support --no-cache build
    
    * #19: Update zgenom plugins to zshrc
    
    * chore(19): Add docker-compose kubectl plugins
    
    * #19: Add commit to edit another computer
    
    * #19(test): Add setPlugins&disableOhMyZshPlugins in zgenom_spec

--- ruby/lib/components/shell/zgenom.rb
+++ ruby/lib/components/shell/zgenom.rb
@@ -13,6 +13,8 @@ module Component
     REPO_URL = "https://github.com/jandamm/zgenom.git"
     TARGET_DIR_PATH = File.join(CONFIG.home, '.zgenom')
 
+    ZSHRC = File.join(CONFIG.home, '.zshrc') # FIXME Get from ZshBinaryComponent
+
     depends_on Component::GitComponent
     depends_on Component::ZshBinaryComponent
 
@@ -31,6 +33,7 @@ module Component
         return
       end
       install!
+      configure()
     end
 
     def install!
@@ -46,6 +49,103 @@ module Component
 
     def rollback
       raise NotImplementedError, 'Rollback not implemented for ZgenomComponent'
+    end
+
+    private
+    def configure
+      disableOhMyZshPlugins
+      setPlugins
+    end
+
+    private
+    def disableOhMyZshPlugins
+      if !File.exist?(ZSHRC)
+        logger.error(".zshrc file not found")
+        raise ".zshrc file not found"
+      end
+
+      zshrc_content = File.read(ZSHRC)
+
+      # FIXME possibly distroy .zshrc
+      # if zshrc_content.gsub!(/^(\s*plugins=\([^)]*\)\s*)$/, '# \1')
+      if zshrc_content.gsub!(/^(\s*plugins=\([^)]*\))/m, '# \1')
+        logger.info("Disabling oh-my-zsh plugins")
+      end
+
+      # FIXME possibly distroy .zshrc
+      #   if zshrc_content.gsub!(/^(\s*source \$ZSH\/oh-my-zsh.sh)$/, '# \1')
+      if zshrc_content.gsub!(/^(\s*source \$ZSH\/oh-my-zsh.sh)/m, '# \1')
+        logger.info("Disabling source oh-my-zsh script")
+      end
+
+      File.open(ZSHRC, 'w') do |file|
+        file.write(zshrc_content)
+      end
+    end
+
+    private
+    def setPlugins
+      if !File.exist?(ZSHRC)
+        logger.error(".zshrc file not found")
+        raise ".zshrc file not found"
+      end
+
+      zshrc_content = File.read(ZSHRC)
+
+      # zgenom autoupdate
+      zgenom_config = ""
+      if zshrc_content.match?(/zgenom autoupdate/)
+        logger.debug("zgenom update already exists in .zshrc, skipping")
+        # Assume already zgenom plugins are set-up
+        return
+      else
+        logger.info("Adding zgenom autoupdate to .zshrc")
+        zgenom_config = <<~CONFIG
+
+          ### Zgenom ###
+          source "${HOME}/.zgenom/zgenom.zsh" > /dev/null
+
+          zgenom autoupdate
+
+          if ! zgenom saved; then
+            # load oh-my-zsh plugins
+            zgenom oh-my-zsh
+            zgenom oh-my-zsh plugins/git
+            zgenom oh-my-zsh plugins/python
+            zgenom oh-my-zsh plugins/systemd
+            zgenom oh-my-zsh plugins/docker
+            zgenom oh-my-zsh plugins/pip
+            zgenom oh-my-zsh plugins/vi-mode
+            zgenom oh-my-zsh plugins/command-not-found
+            zgenom oh-my-zsh plugins/docker-compose
+            zgenom oh-my-zsh plugins/kubectl
+            
+            # load zsh-users plugins
+            zgenom load zsh-users/zsh-syntax-highlighting
+            zgenom load zsh-users/zsh-history-substring-search
+            zgenom load zsh-users/zsh-autosuggestions
+            zgenom load zsh-users/zsh-completions
+
+            # load zsh-autoswitch-virtualenv
+            zgenom load MichaelAquilina/zsh-autoswitch-virtualenv
+
+            # load nvm
+            zgenom load lukechilds/zsh-nvm
+
+            # load fzf-tab
+            zgenom load Aloxaf/fzf-tab 
+            
+            # Save and compile .zshrc
+            zgenom save
+            zgenom compile "$HOME/.zshrc"
+          fi
+        CONFIG
+      end
+
+      zshrc_content << zgenom_config
+      File.open(ZSHRC, 'w') do |file|
+        file.write(zshrc_content)
+      end
     end
   end
 end
--- ruby/spec/lib/components/shell/zgenom_spec.rb
+++ ruby/spec/lib/components/shell/zgenom_spec.rb
@@ -63,6 +63,8 @@ RSpec.describe Component::ZgenomComponent do
         allow(mock_zsh_binary).to receive(:available?).and_return(true)
         allow(mock_git).to receive(:clone).with(described_class::REPO_URL, described_class::TARGET_DIR_PATH)
         
+        allow(zgenom).to receive(:configure).and_return(true)
+
         # Let's spy on the dependencies method
         allow(zgenom).to receive(:dependencies).and_call_original
 
@@ -71,6 +73,90 @@ RSpec.describe Component::ZgenomComponent do
         expect(mock_git)
           .to have_received(:clone)
           .with(described_class::REPO_URL, described_class::TARGET_DIR_PATH)
+      end
+    end
+  end
+
+  describe '#disableOhMyZshPlugins' do
+    it 'disables oh-my-zsh plugins' do
+      original_content = <<~CONTENT
+        source $ZSH/oh-my-zsh.sh
+        plugins=(git docker)
+        
+        something else
+        another thing
+      CONTENT
+
+      expected_final_content = <<~CONTENT
+        # source $ZSH/oh-my-zsh.sh
+        # plugins=(git docker)
+
+        something else
+        another thing
+      CONTENT
+
+      # mocking file
+      file_mock = double('file', write: true)
+      allow(file_mock).to receive(:write)
+      
+      # Given
+      allow(File).to receive(:exist?).with(described_class::ZSHRC).and_return(true)
+      allow(File).to receive(:read).with(described_class::ZSHRC).and_return(original_content)
+      allow(File).to receive(:open).with(described_class::ZSHRC, 'w').and_yield(file_mock)
+      
+      zgenom.send(:disableOhMyZshPlugins)
+
+      expect(File).to have_received(:open).with(described_class::ZSHRC, 'w')
+      expect(file_mock).to have_received(:write).with(expected_final_content)
+    end
+  end
+
+  describe '#setPlugins' do
+    
+    it 'Already zgenom autoupdate exists in .zshrc, skip' do
+      content = <<~CONTENT
+        Something bla
+        # plugins=(git docker)
+
+        something else
+        zgenom autoupdate
+
+        Hello world!
+        PATH="$PATH"
+      CONTENT
+
+      allow(File).to receive(:exist?).with(described_class::ZSHRC).and_return(true)
+      allow(File).to receive(:read).with(described_class::ZSHRC).and_return(content)
+      
+      expect(File).not_to receive(:open).with(described_class::ZSHRC, 'w')
+
+      zgenom.send(:setPlugins)
+    end
+
+    it 'set zgenom pluings' do
+      content = <<~CONTENT
+        Something blah
+        # plugins=(git docker)
+
+        something else
+
+        Hello world!
+        PATH="$PATH"
+      CONTENT
+
+      # mocking file
+      file_mock = double('file', write: true)
+      allow(file_mock).to receive(:write) 
+
+      allow(File).to receive(:exist?).with(described_class::ZSHRC).and_return(true)
+      allow(File).to receive(:read).with(described_class::ZSHRC).and_return(content)
+      allow(File).to receive(:open).with(described_class::ZSHRC, 'w').and_yield(file_mock)
+
+      zgenom.send(:setPlugins)
+
+      expect(file_mock).to have_received(:write) do
+        expect(content).to match(/zgenom autoupdate/)
+        expect(content).to match(/Something blah/)
       end
     end
   end
