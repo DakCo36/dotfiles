c0bfb50d477dcfcefcb770928e0f1a686d06b003
Wonjae <wonjae.kim@bucketplace.net>
2026-01-29 11:21:44 +0900

    docs: add test code writing guidelines
    - Add Given/When/Then pattern requirement for test organization
    - Minimize inline comments in test code
    - Include good/bad examples for clarity
    - Remove redundant comment from zgenom_spec.rb

--- devkit/spec/lib/components/shell/zgenom_spec.rb
+++ devkit/spec/lib/components/shell/zgenom_spec.rb
@@ -67,7 +67,6 @@ RSpec.describe Component::ZgenomComponent do
         allow(mock_zsh_binary).to receive(:available?).and_return(true)
         allow(mock_git).to receive(:clone).with(described_class::REPO_URL, described_class::TARGET_DIR_PATH)
 
-        # Mock FileUtils to prevent actual file system operations
         allow(Dir).to receive(:exist?).with(described_class::TARGET_DIR_PATH).and_return(false)
         allow(FileUtils).to receive(:rm_rf).and_return(nil)
         allow(FileUtils).to receive(:mkdir_p).and_return(nil)
