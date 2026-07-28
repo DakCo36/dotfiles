3140fc3ee8a1d71e2b9734d5f046f3ab2e5b6ba5
Wonjae <wonjae.kim@bucketplace.net>
2026-07-28 18:40:13 +0900

    [Devkit] feat(emacs): More empathesize matching parenthesis

--- devkit/resources/emacs/lisp/appearance.el
+++ devkit/resources/emacs/lisp/appearance.el
@@ -11,4 +11,22 @@
   (scroll-bar-mode -1)      ;; Remove scrollbar
   (set-fringe-mode 8))      ;; Add padding left/right
 
+;; Matching paren: theme colors + bold/underline on the paren chars.
+(show-paren-mode 1)
+(setq show-paren-delay 0
+      show-paren-style 'parenthesis
+      show-paren-highlight-openparen t
+      show-paren-when-point-inside-paren t
+      show-paren-when-point-in-periphery t)
+
+(defun init-appearance-apply-paren-faces (&rest _)
+  (set-face-attribute 'show-paren-match nil
+                      :weight 'ultra-bold
+                      :underline t)
+  (set-face-attribute 'show-paren-mismatch nil
+                      :weight 'ultra-bold
+                      :underline t))
+
+(add-hook 'enable-theme-functions #'init-appearance-apply-paren-faces)
+
 (provide 'appearance)
